feb9a06a9f63ad10d3aaa79347cbe0a902ed6a66
NguyenAnhTuan1912 <nguyenanhtuan19122002@gmail.com>
2024-11-29 14:17:42 +0700

    Update scripts

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
-# Note: please run this file with sudo command
+# Note: please run this file with sudo command or run as Root user
 # Note: this script is designed for Ubuntu
 
 WORK_DIR=s3-examples
 ROOT_TO_WORK_DIR=/opt/$WORK_DIR
+UBUNTU_USER_DIR=/home/ubuntu
 
-## Move to /opt
-cd /opt
-
-## Get new version of application from github
-git clone https://github.com/Code4life-Labs/s3-examples.git
-cd $WORK_DIR
+## Change CD to WORK_DIR in ubuntu
+cd $HOME/$WORK_DIR
 
 ## Install dependencies
 npm install
 
+## Remove old install on /opt
+sudo rm -r $ROOT_TO_WORK_DIR
+
+## Copy to /opt
+cd ~
+sudo cp -R $UBUNTU_USER_DIR/$WORK_DIR $WORK_DIR /opt
+cd $ROOT_TO_WORK_DIR
+
 ## Setup services
-cp $ROOT_TO_WORK_DIR/setup/s3examples.service > /etc/systemd/system
+sudo sed -i "s|NODE_PATH|$(which node)|g" $ROOT_TO_WORK_DIR/setup/s3examples.service
+sudo cp $ROOT_TO_WORK_DIR/setup/s3examples.service /etc/systemd/system
 
 ## Start service
-systemctl daemon-reload
-systemctl start s3examples.service
+sudo systemctl daemon-reload
+sudo systemctl start s3examples.service
 
 ## Enable service
-systemctl enable s3examples.service
+sudo systemctl enable s3examples.service
 
 ## Check status
-systemctl status s3examples.service
+sudo systemctl status s3examples.service
--- src/services/aws-s3/__config__.js
+++ src/services/aws-s3/__config__.js
@@ -2,10 +2,14 @@ const { S3Client } = require("@aws-sdk/client-s3");
 
 // S3 Client will be setup here
 const s3Client = new S3Client({
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-  },
+  // If you want to use your credentials, uncomment these lines
+  // And define your `AWS_ACCESS_KEY` and `AWS_SECRET_KEY`
+  // in .env file at root of repository
+  //
+  // credentials: {
+  //   accessKeyId: process.env.AWS_ACCESS_KEY,
+  //   secretAccessKey: process.env.AWS_SECRET_KEY,
+  // },
 });
 
 module.exports = {
